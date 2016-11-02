d2e064ae84ed7c195324df6f01793703b2c989bc
Kern <212585850@logon.ds.ge.com>
2016-11-02 15:07:01 +0100

    wololo

--- src/js/practice.js
+++ src/js/practice.js
@@ -1,5 +1,29 @@
 var changeNumbers = document.getElementById("numberSelect");
 
+var t1 = [{
+    "id": 2,
+    "name": "fesfff"
+},
+    {
+        "id": 10,
+        "name": "fefsdfsxf"
+    },
+    {
+        "id": 22,
+        "name": "fegwsdgf"
+    },
+    {
+        "id": 33,
+        "name": "fsdgsef"
+    },
+    {
+        "id": 101,
+        "name": "fefwf"
+    }];
+var at = {};
+t1.forEach(function (e) {
+    at[e.id] = e;
+});
 function writeNumbers(n)
 {
     for (var i = 0; i < n; i++) {
@@ -7,6 +31,31 @@ function writeNumbers(n)
     }
 }
 writeNumbers(20);
+
+function isEqual() {
+    if(0 === new String("0")) {
+        document.write("0 === new String(0):   true<br/>");
+    }
+    else {
+        document.write("0 === new String(0):   false<br/>");
+    }
+    if(null == undefined) {
+        document.write("null == undefined:   true<br/>");
+    }
+    else {
+        document.write("null == undefined:   false<br/>");
+    }
+    if(null == false) {
+        document.write("null == false:   true<br/>");
+    }
+    else {
+        document.write("null == false:   false<br/>");
+    }
+
+    document.write("at[101]: " + JSON.stringify(at[101]));
+}
+isEqual();
+
 
 changeNumbers.addEventListener("click", function() {
     var options = changeNumbers.querySelectorAll("option");
