3a9b7d8f8837a122ec0e6a0e4b5599684824bf60
Bene <212585846@logon.ds.ge.com>
2016-11-04 14:51:10 +0100

    dataTable stuff almost working (no design)

--- src/js/jsonDataLoading.js
+++ src/js/jsonDataLoading.js
@@ -15,34 +15,41 @@ function loadJson(filename) {
     $.ajax({
         url: '../../json/' + filename + '.json',
         success: function (result) {
-            console.log(result);
-            $('#example').DataTable({
-                data: result,
+            $('#example').dataTable({
+                data : result,
                 columns: [
-                    {data: "index"},
-                    {data: "name"},
-                    {data: "eyeColor"},
-                    {data: "age"},
-                    {data: "gender"},
-                    {data: "about"}
-                ]
-            });
+                    { data: "index"},
+                    { data: "name" },
+                    { data: "age" },
+                    { data: "gender" },
+                    { data: "eyeColor" },
+                    { data: "about" }
+                    ]
+            })
         },
-        async: false
-    })
+        async: true
+    });
 }
 
 
-// for (var i = 0; i < result.length; i++) {
-//     var obj = result[i];
-//     table.append("<tr>" +
-//         "<td style=\"border:1px solid black\">" + obj.index + "</td>" +
-//         "<td style=\"border:1px solid black\">" + obj.name + "</td>" +
-//         "<td style=\"border:1px solid black\">" + obj.age + "</td>" +
-//         "<td style=\"border:1px solid black\">" + obj.eyeColor + "</td>" +
-//         "<td style=\"border:1px solid black\">" + obj.gender + "</td>" +
-//         "<td style=\"border:1px solid black\">" + obj.about + "</td>" +
-//         "</tr>");
+// function loadJson(filename) {
+//     $.ajax({
+//         url: '../../json/' + filename + '.json',
+//         success: function (result) {
+//             $('#dynamictable').append('<table style="border:1px solid black"></table>');
+//             var table = $('#dynamictable').children();
+//             for (var i = 0; i < result.length; i++) {
+//                 var obj = result[i];
+//                 table.append("<tr>" +
+//                     "<td style=\"border:1px solid black\">" + obj.index + "</td>" +
+//                     "<td style=\"border:1px solid black\">" + obj.name + "</td>" +
+//                     "<td style=\"border:1px solid black\">" + obj.age + "</td>" +
+//                     "<td style=\"border:1px solid black\">" + obj.eyeColor + "</td>" +
+//                     "<td style=\"border:1px solid black\">" + obj.gender + "</td>" +
+//                     "<td style=\"border:1px solid black\">" + obj.about + "</td>" +
+//                     "</tr>");
+//             }
+//         },
+//         async: true
+//     });
 // }
-//
-// },
