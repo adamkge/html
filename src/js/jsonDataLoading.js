67a72e6046812bbce268933b16a12379862b410b
Bene <212585846@logon.ds.ge.com>
2016-11-04 14:39:52 +0100

    m10;70Rr majdnem datatables szepseg

--- src/js/jsonDataLoading.js
+++ src/js/jsonDataLoading.js
@@ -4,9 +4,9 @@
 
 $(function () {
         loadJson("json10");
-        loadJson("json100");
-        loadJson("json1000");
-        loadJson("json10000");
+        // loadJson("json100");
+        // loadJson("json1000");
+        // loadJson("json10000");
     }
 );
 
@@ -15,23 +15,34 @@ function loadJson(filename) {
     $.ajax({
         url: '../../json/' + filename + '.json',
         success: function (result) {
-
-            $('#dynamictable').append('<table style="border:1px solid black"></table>');
-            var table = $('#dynamictable').children();
-
-            for (var i = 0; i < result.length; i++) {
-                var obj = result[i];
-                table.append("<tr>" +
-                    "<td style=\"border:1px solid black\">" + obj.index + "</td>" +
-                    "<td style=\"border:1px solid black\">" + obj.name + "</td>" +
-                    "<td style=\"border:1px solid black\">" + obj.age + "</td>" +
-                    "<td style=\"border:1px solid black\">" + obj.eyeColor + "</td>" +
-                    "<td style=\"border:1px solid black\">" + obj.gender + "</td>" +
-                    "<td style=\"border:1px solid black\">" + obj.about + "</td>" +
-                    "</tr>");
-            }
-
+            console.log(result);
+            $('#example').DataTable({
+                data: result,
+                columns: [
+                    {data: "index"},
+                    {data: "name"},
+                    {data: "eyeColor"},
+                    {data: "age"},
+                    {data: "gender"},
+                    {data: "about"}
+                ]
+            });
         },
         async: false
-    });
+    })
 }
+
+
+// for (var i = 0; i < result.length; i++) {
+//     var obj = result[i];
+//     table.append("<tr>" +
+//         "<td style=\"border:1px solid black\">" + obj.index + "</td>" +
+//         "<td style=\"border:1px solid black\">" + obj.name + "</td>" +
+//         "<td style=\"border:1px solid black\">" + obj.age + "</td>" +
+//         "<td style=\"border:1px solid black\">" + obj.eyeColor + "</td>" +
+//         "<td style=\"border:1px solid black\">" + obj.gender + "</td>" +
+//         "<td style=\"border:1px solid black\">" + obj.about + "</td>" +
+//         "</tr>");
+// }
+//
+// },
