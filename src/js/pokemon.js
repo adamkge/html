7bf96f35361c8d4826b9102ac6734b1b0888b1e2
Istvan Panczel <istvan.panczel@ge.com>
2016-11-10 13:14:42 +0100

    pokemon improved, id or name sort

--- src/js/pokemon.js
+++ src/js/pokemon.js
@@ -1,52 +1,98 @@
 var myTable = document.getElementById('table');
 var rowTemplate = "<tr><td>{0}</td><td>{1}</td><td>{2}</td></tr>";
-$( document ).ready(function() {
+$(document).ready(function () {
     $.ajax({
         url: '//pokeapi.co/api/v2/pokemon/?limit=151',
         success: function (result) {
             var pokemons = [];
             for (var i = 0; i < result.results.length; i++) {
-                pokemons.push({name : result.results[i].name.charAt(0).toUpperCase() + result.results[i].name.slice(1),
-                    url: "//raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"+(i+1)+".png",
-                    id : (i+1)});
-                console.log(pokemons[i]);
+                pokemons.push({
+                    name: result.results[i].name.charAt(0).toUpperCase() + result.results[i].name.slice(1),
+                    url: "//raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + (i + 1) + ".png",
+                    id: (i + 1)
+                });
             }
-            for(var i = 0; i < pokemons.length; i++) {
-/*                var opt = pokemons[i].name;
-                var tr = document.createElement("tr");
-                var td = document.createElement("td");
-                var id = document.createElement("text");
-                id.textContent = pokemons[i].id;
-                id.value = pokemons[i].id;
-                el.appendChild(id);
+
+            // myTable.innerHTML = "";
+            while (myTable.firstChild)
+            {
+                myTable.removeChild(myTable.firstChild);
+            }
+            for (var i = 0; i < pokemons.length; i++) {
+
+                var tbody = document.createElement('tbody');
+                var tr = document.createElement('tr');
+                var td1 = document.createElement('td');
+                var td2 = document.createElement('td');
+                var td3 = document.createElement('td');
+                var text1 = document.createTextNode(pokemons[i].id);
+                var text2 = document.createTextNode(pokemons[i].name);
                 var img = document.createElement("img");
                 img.src = pokemons[i].url;
-                el.appendChild(img);
-                myList.appendChild(el);*/
-
-                $("#table").append(
-                    rowTemplate.format("pokemons[i].id", "pokemons[i].name", "<img src=" +pokemons[i].url + "/>")
-                );
+                td1.className = "idtd";
+                td1.appendChild(text1);
+                tr.appendChild(td1);
+                td2.className = "nametd";
+                td2.appendChild(text2);
+                tr.appendChild(td2);
+                td3.className = "imagetd";
+                td3.appendChild(img);
+                tr.appendChild(td3);
+                tbody.appendChild(tr);
+                myTable.appendChild(tbody);
             }
         },
         async: true
     });
 
-    $('#select').on('change', function() {
-        var items = $('#list li').get();
-        items.sort(function(a,b){
-            var keyA = $(a).text();
-            var keyB = $(b).text();
+    $('#select').on('change', function () {
+        var rows = $('#table > tbody > tr').get();
 
-            if (keyA < keyB) return -1;
-            if (keyA > keyB) return 1;
-            return 0;
-        });
-        var ul = $('#list');
-        $.each(items, function(i, li){
-            ul.append(li);
-        });
+        var optionSelected = $("option:selected", this);
+        var valueSelected = this.value;
+        console.log(optionSelected);
+        console.log(valueSelected);
+
+        var f;
+        if (valueSelected === "id") {
+            console.log("if: id");
+            f = idsort;
+        }
+        else {
+            console.log("if: name");
+            f = namesort
+        }
+
+        rows.sort(f);
+        console.log(rows);
+        while (myTable.firstChild)
+        {
+            myTable.removeChild(myTable.firstChild);
+        }
+        console.log("after delete");
+        console.log(rows);
+        for (var i = 0; i < rows.length; i++) {
+            $('#table').append(rows[i]);
+        }
     });
 });
 
+
+function idsort(a, b) // num
+{
+    console.log("idsort called")
+    var anum = parseInt($(a).find(".idtd").text(), 10);
+    var bnum = parseInt($(b).find(".idtd").text(), 10);
+    return anum - bnum;
+}
+
+function namesort(a, b) // string
+{
+    console.log("namesort called")
+    var keyA = $(a).find(".nametd").text();
+    var keyB = $(b).find(".nametd").text();
+    if (keyA < keyB) return -1;
+    if (keyA > keyB) return 1;
+    return 0;
+}
 
