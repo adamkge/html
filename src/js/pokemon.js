8c824b5149d79ce5daed107d4c29ecf796ce6a30
Kern <212585850@logon.ds.ge.com>
2016-11-10 11:05:38 +0100

    appendchild

--- src/js/pokemon.js
+++ src/js/pokemon.js
@@ -1,28 +1,52 @@
-var myList = document.getElementById('list');
+var myTable = document.getElementById('table');
+var rowTemplate = "<tr><td>{0}</td><td>{1}</td><td>{2}</td></tr>";
 $( document ).ready(function() {
     $.ajax({
         url: '//pokeapi.co/api/v2/pokemon/?limit=151',
         success: function (result) {
             var pokemons = [];
-            var images = [];
             for (var i = 0; i < result.results.length; i++) {
-                pokemons.push(result.results[i].name);
-                images.push("//raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"+(i+1)+".png");
+                pokemons.push({name : result.results[i].name.charAt(0).toUpperCase() + result.results[i].name.slice(1),
+                    url: "//raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"+(i+1)+".png",
+                    id : (i+1)});
+                console.log(pokemons[i]);
             }
             for(var i = 0; i < pokemons.length; i++) {
-                var opt = pokemons[i];
-                var el = document.createElement("li");
-                el.textContent = opt;
-                el.value = opt;
-                myList.appendChild(el);
-
+/*                var opt = pokemons[i].name;
+                var tr = document.createElement("tr");
+                var td = document.createElement("td");
+                var id = document.createElement("text");
+                id.textContent = pokemons[i].id;
+                id.value = pokemons[i].id;
+                el.appendChild(id);
                 var img = document.createElement("img");
-                img.src = images[i];
-                myList.appendChild(img);
+                img.src = pokemons[i].url;
+                el.appendChild(img);
+                myList.appendChild(el);*/
+
+                $("#table").append(
+                    rowTemplate.format("pokemons[i].id", "pokemons[i].name", "<img src=" +pokemons[i].url + "/>")
+                );
             }
-            console.log(pokemons[0]);
-            //$('body').text(JSON.stringify(result));
         },
         async: true
     });
+
+    $('#select').on('change', function() {
+        var items = $('#list li').get();
+        items.sort(function(a,b){
+            var keyA = $(a).text();
+            var keyB = $(b).text();
+
+            if (keyA < keyB) return -1;
+            if (keyA > keyB) return 1;
+            return 0;
+        });
+        var ul = $('#list');
+        $.each(items, function(i, li){
+            ul.append(li);
+        });
+    });
 });
+
+
