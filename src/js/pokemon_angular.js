e6ef5ecd65d1c10a5b225d033acfad6ac06468d6
Istvan Panczel <istvan.panczel@ge.com>
2016-11-11 16:30:27 +0100

    clikc on pokemon gets the attributes (results only on console now)

--- src/js/pokemon_angular.js
+++ src/js/pokemon_angular.js
@@ -1,11 +1,11 @@
 var app = angular.module("pokeModule", []);
-app.controller("myCtrl", function($scope, $http) {
+app.controller("myCtrl", function ($scope, $http) {
     var pokemons = [];
     $scope.orderBy = 'name';
-    $scope.sortingOrder  = false;
+    $scope.sortingOrder = false;
 
     $http.get("//pokeapi.co/api/v2/pokemon/?limit=151", {jsonpCallbackParam: 'callback'})
-        .success(function(data){
+        .success(function (data) {
             for (var i = 0; i < data.results.length; i++) {
                 pokemons.push({
                     name: data.results[i].name.charAt(0).toUpperCase() + data.results[i].name.slice(1),
@@ -16,9 +16,17 @@ app.controller("myCtrl", function($scope, $http) {
             console.log(pokemons);
             //console.log(data.found);
         });
-    $scope.pokemons=pokemons;
+    $scope.pokemons = pokemons;
 
     $scope.names = ["Id", "Name"];
+
+    $scope.go = function(item)
+    {
+        $http.get("//pokeapi.co/api/v2/pokemon/" + item.id, {jsonpCallbackParam: 'callback'})
+            .success(function (data) {
+                console.log(data);
+            });
+    };
 
 });
 
